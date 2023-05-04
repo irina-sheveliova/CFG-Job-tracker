9ebf0ffc91ec3b9f15c6210d350776e1f055e3a2
Oliviao123 <oliviaonu@outlook.com>
2023-05-04 20:20:09 +0100

    Removed Dash, Contact us, How it works Import statements from App.js file

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import About from './About';
-import Howitworks from './Howitworks'
-import Contactus from './Contactus'
-import Dashboard from './Dashboard'
 
 function App() {
   return (
