3459793ca6dcea22ec0b77c9a768968ae8cbf3e9
Oliviao123 <oliviaonu@outlook.com>
2023-05-05 14:29:55 +0100

    Landing page update

--- src/App.js
+++ src/App.js
@@ -11,23 +11,25 @@ function App() {
     <Router>
       <div>
         <Nav />
-        <div className="landing-page">
-          <div className="landing-page__content">
-            <h5 className="landing-page__title">Job Flow</h5>
-            <h3 className="landing-page__subtitle">Stay organised and track your job applications for free today</h3>
-            <div className="landing-page__buttons">
-              <button className="landing-page__button">Login</button>
-              <button className="landing-page__button">Sign Up</button>
-            </div>
-          </div>
-          <div className="logo">
-            <img src={jobflowlogo} alt="Job Flow logo" />
-          </div>
-          <div className="landing-pic">
-            <img src={landingpic} alt="landing pic" />
-          </div>
-        </div>
         <Routes>
+          <Route path="/" element={
+            <div className="landing-page">
+              <div className="landing-page__content">
+                <h5 className="landing-page__title">Job Flow</h5>
+                <h3 className="landing-page__subtitle">Stay organised and track your job applications for free today</h3>
+                <div className="landing-page__buttons">
+                  <button className="landing-page__button">Login</button>
+                  <button className="landing-page__button">Sign Up</button>
+                </div>
+              </div>
+              <div className="logo">
+                <img src={jobflowlogo} alt="Job Flow logo" />
+              </div>
+              <div className="landing-pic">
+                <img src={landingpic} alt="landing pic" />
+              </div>
+            </div>
+          } />
           <Route path="/about" element={<About />} />
           <Route path="/signup" element={<RegistrationPage />} />
         </Routes>
@@ -37,5 +39,6 @@ function App() {
 }
 
 export default App;
+
 
 
