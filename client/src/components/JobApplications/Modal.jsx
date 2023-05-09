5d85630079a98e79fee0400aa61df2cae25bcdb9
ebfaler <74653573+ebfaler@users.noreply.github.com>
2023-05-09 16:30:35 +0200

    modal styling first draft done

--- client/src/components/JobApplications/Modal.jsx
+++ client/src/components/JobApplications/Modal.jsx
@@ -8,39 +8,42 @@ function JobModal() {
             <div className="modal">
 
                 <form>
-                    <div>
-                        <label htmlFor="job_position">Job Position</label>
-                        <input type="text" name="job_position" />
-                    </div>
-                    <div>
-                        <label htmlFor="company"> Company</label>
-                        <input type="text" name="company" />
-                    </div>
-                    <div>
-                        <label htmlFor="doa"> Date of Application</label>
-                        <input type="date" name="doa" />
-                    </div>
+                    <div className="main-div">
+                        <div className="form-div">
+                            <label htmlFor="job_position">Job Position</label>
+                            <input type="text" name="job_position" />
+                        </div>
+                        <div className="form-div">
+                            <label htmlFor="company"> Company</label>
+                            <input type="text" name="company" />
+                        </div>
+                        <div className="form-div">
+                            <label htmlFor="doa"> Date of Application</label>
+                            <input type="date" name="doa" />
+                        </div>
 
-                    <div>
-                        <label htmlFor="status"> Status</label>
-                        {/* <input name="status" /> */}
-                        <select name="status">
-                            <option value="applied">Applied</option>
-                            <option value="no-applied">Not Applied</option>
-                            <option value="Applied">Interviewings</option>
-                        </select>
-                    </div>
-                    <div>
+                        <div className="form-div">
+                            <label htmlFor="status"> Status</label>
+                            {/* <input name="status" /> */}
+                            <select name="status">
+                                <option value="applied">Applied</option>
+                                <option value="no-applied">Not Applied</option>
+                                <option value="interviewing">Interviewing</option>
+                                <option value="offer">Offer</option>
+                                <option value="rejected">Rejected</option>
+                            </select>
+                        </div>
+                        <div className="form-div">
+                            <label htmlFor="salary"> Salary</label>
+                            <input name="salary" />
+                        </div>
+                        <div className="form-div">
 
-                        <label htmlFor="salary"> Salary</label>
-                        <input name="salary" />
+                            <label htmlFor="notes"> Notes</label>
+                            <textarea id="notes" type="text" name="notes"></textarea>
+                        </div>
+                        <button className="modal-button" type="submit">Enter</button>
                     </div>
-                    <div>
-
-                        <label htmlFor="notes"> Notes</label>
-                        <input type="text" name="notes" />
-                    </div>
-
                 </form>
             </div>
         </div>
