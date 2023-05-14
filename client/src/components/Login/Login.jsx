b177b433183424387a3bf670395a33da93144e81
Irina Șeveliova <52661627+irina-sheveliova@users.noreply.github.com>
2023-05-14 17:01:00 +0200

    Updated Firebase structure + login

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,92 +1,95 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase';
+import { useNavigate } from 'react-router-dom';
 import "./Login.css";
 
 function Login() {
-    // React States
-    const [errorMessages, setErrorMessages] = useState({});
-    const [isSubmitted, setIsSubmitted] = useState(false);
-  
-    // User Login info
-    const database = [
-      {
-        username: "user1@test.com",
-        password: "user1@test.com"
-      },
-      {
-        username: "user2@test.com",
-        password: "user2@test.com"
-      }
-    ];
-  
-    const errors = {
-      uname: "Username not found",
-      pass: "Invalid password"
-    };
-  
-    const onSubmit = (event) => {
-      //Prevent page reload
-      event.preventDefault();
-  
-      let { uname, pass } = document.forms[0];
-  
-      // Find user login info
-      const userData = database.find((user) => user.username === uname.value);
-  
-      // Compare user info
-      if (userData) {
-        if (userData.password !== pass.value) {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessages, setErrorMessages] = useState({});
+
+  const errors = {
+    userNotFound: "User not found",
+    invalidPass: "Invalid password",
+    invalidEmail: "Please enter a valid email address"
+  };
+
+  const onLogin = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate("/applications")
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        if (errorCode === 'auth/wrong-password') {
           // Invalid password
-          setErrorMessages({ name: "pass", message: errors.pass });
-        } else {
-          setIsSubmitted(true);
+          setErrorMessages({ name: "invalidPass", message: errors.invalidPass });
+        } else if (errorCode === 'auth/user-not-found') {
+          // User not found
+          setErrorMessages({ name: "userNotFound", message: errors.userNotFound });
+        } else if (errorCode === 'auth/invalid-email') {
+          // Invalid email
+          setErrorMessages({ name: "invalidEmail", message: errors.invalidEmail });
         }
-      } else {
-        // Username not found
-        setErrorMessages({ name: "uname", message: errors.uname });
-      }
-    };
-  
-    // Generate JSX code for error message
-    const renderErrorMessage = (name) =>
-      name === errorMessages.name && (
-        <div className="form-error">{errorMessages.message}</div>
-      );
-  
-    // JSX code for login form
-    const renderForm = (
-      <form onSubmit={onSubmit}>
-        <div className="form-container">
-          <h2 className="login-form-header">Log in</h2>
-          <div className="form-group">
-            <input
-              type="email"
-              name="uname" required 
-              placeholder="Email"/>
-            {renderErrorMessage("uname")}
-          </div>
-          <div className="form-group">
-            <input 
-              type="password" 
-              name="pass" required 
-              placeholder="Password"/>
-            {renderErrorMessage("pass")}
-          </div>
-          <div className="button-container">
-            <button type="submit">Continue</button>
-          </div>
-          <p>Don't have an account? <a href="/signup">Sign Up</a></p>
-        </div>
-      </form>
+        else {
+          alert(errorMessage);
+        }
+        console.log(errorCode, errorMessage)
+      });
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="form-error">{errorMessages.message}</div>
     );
-  
-    return (
-      <div className="app">
-        <div className="login-form">
-          {isSubmitted ? <h3 className="loginMessage">user logged in successfully</h3> : renderForm}
+
+  return (
+    <form>
+      <div className="form-container">
+        <h2 className="login-form-header">Log in</h2>
+        <div className="form-group">
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="Email"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          {renderErrorMessage("userNotFound")}
+          {renderErrorMessage("invalidEmail")}
         </div>
+
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          {renderErrorMessage("invalidPass")}
+        </div>
+
+        <div className="form-group">
+          <button type="submit" style={{ width: "100%" }}
+            onClick={onLogin}>Continue</button>
+        </div>
+        <p style={{ textAlign: "center", fontSize: "18px" }}>
+          Don't have an account? <a href="/signup">Sign Up</a></p>
+
       </div>
-    );
-  }
-  
-  export default Login;
+    </form>
+  );
+}
+
+export default Login;
