c1d5e3b0df5b3575655e701c2fed8051b1fa4acf
Olivia Onu <128523943+oliviao123@users.noreply.github.com>
2023-05-15 20:49:34 +0100

    Title added to Job Search Page

--- client/src/components/JobSearch/search.js
+++ client/src/components/JobSearch/search.js
@@ -49,6 +49,8 @@ const Search = () => {
 
   return (
     <div>
+     <h2 id="search-title">Unleash Your Career Potential with <span id="jobflow-text">JobFlow</span></h2>
+     <h2 id= "sub-search-title">Find Your <span id="Dream-sub">Dream</span> Job Today!</h2>
       <form onSubmit={searchJsearch}>
         <input type="text" value={query} onChange={(event) => setQuery(event.target.value)} placeholder="Job, Company, Keywords" />
         <button type="submit">Search</button>
@@ -88,4 +90,5 @@ const Search = () => {
 };
 
 export default Search;
+
 
