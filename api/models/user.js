5e31f5ebabf85a135060cecb4dd492c65fef8651
ebfaler <74653573+ebfaler@users.noreply.github.com>
2023-05-17 15:48:52 +0200

    set 'allowNull' to false on user model

--- api/models/user.js
+++ api/models/user.js
@@ -1,16 +1,20 @@
 export default (sequelize, Sequelize) => {
     const User = sequelize.define('user', {
         UID: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false
         },
         firstName: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false
         },
         lastName: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false
         },
         email: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false
         }
     });
 
