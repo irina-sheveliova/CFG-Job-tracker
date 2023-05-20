8aea8180e65ff9dbe23b8b0c308aa445223119d1
Joy <remen.omodiale@gmail.com>
2023-05-20 22:30:52 +0100

    added logout navlink - removed applications nav link - trying userId useContext for applications

--- api/models/job.js
+++ api/models/job.js
@@ -2,29 +2,33 @@
 // 'doa'stands for 'date of application'
 
 export default (sequelize, Sequelize) => {
-    const Job = sequelize.define('job', {
-        position: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        company: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        doa: {
-            type: Sequelize.STRING
-        },
-        salary: {
-            type: Sequelize.STRING
-        },
-        status: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        notes: {
-            type: Sequelize.TEXT
-        }
-    });
+  const Job = sequelize.define("job", {
+    position: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    company: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    doa: {
+      type: Sequelize.STRING,
+    },
+    salary: {
+      type: Sequelize.STRING,
+    },
+    status: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    notes: {
+      type: Sequelize.TEXT,
+    },
+    //   userId: {
+    //     type: Sequelize.STRING, // Assuming userId is a string
+    //     allowNull: false,
+    //   },
+  });
 
-    return Job;
+  return Job;
 };
