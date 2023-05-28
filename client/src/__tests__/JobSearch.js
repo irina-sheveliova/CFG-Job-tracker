dc67491b7173acc54114751e99857736a0868cbf
Katarzyna Kaczmarek <kaczmarekkasia.mail@gmail.com>
2023-05-28 15:03:15 +0200

    unit tests for login, registration, jobsearch

--- client/src/__tests__/JobSearch.js
+++ client/src/__tests__/JobSearch.js
@@ -1,6 +1,33 @@
-import { render } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import axios from 'axios';
 import Search from '../components/JobSearch/search';
 
-test('Renders "Search for Jobs" page', () => {
-  render(<Search />);
+jest.mock('axios');
+
+describe('Search component', () => {
+  test('Renders "Search for Jobs" page', () => {
+    render(<Search />);
+  });
+  test('performs search correctly', async () => {
+    axios.get.mockResolvedValueOnce();
+
+    render(<Search />);
+
+    const searchInput = screen.getByPlaceholderText(
+      'Please Enter The Desired Job Title and Location e.g. Software Engineer in London'
+    );
+    const searchButton = screen.getByText('Search');
+
+    fireEvent.change(searchInput, {
+      target: { value: 'Software Engineer in London' },
+    });
+    fireEvent.click(searchButton);
+
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith(
+      'https://jsearch.p.rapidapi.com/search',
+      expect.any(Object)
+    );
+  });
 });
